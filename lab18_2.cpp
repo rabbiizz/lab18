9d9cf01136fb431e98dbe33ba727faa5b3604596
rabbiizz <Supakarn_k@cmu.ac.th>
2024-02-13 15:18:24 +0700

    lab18_2

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,16 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+    double sqx1 = (**&R1).w + (**&R1).x;
+	double sqx2 = (**&R2).w + (**&R2).x;
 
+	double sqy1 = (**&R1).y - (**&R1).h;
+	double sqy2 = (**&R2).y - (**&R2).h;
 
-
+	double side1, side2;
+	side1 = min(sqx1, sqx2) - max((**&R1).x, (**&R2).x);
+	side2 = min((**&R1).y, (**&R2).y) - max(sqy1, sqy2);
+	if (side1 * side2 < 0) return 0;
+	else return side1 * side2;
 }
